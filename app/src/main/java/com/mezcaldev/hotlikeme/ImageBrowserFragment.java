7a94f4856de136c2f79795e3e456c6af1ecfb6a4
gfMX <grafica.mx@gmail.com>
2016-06-22 12:41:19 -0500

    Image browser

--- app/src/main/java/com/mezcaldev/hotlikeme/ImageBrowserFragment.java
+++ app/src/main/java/com/mezcaldev/hotlikeme/ImageBrowserFragment.java
@@ -2,11 +2,16 @@ package com.mezcaldev.hotlikeme;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,11 +21,12 @@ public class ImageBrowserFragment extends Fragment {
     private static final String TAG = "FacebookLogin";
 
     public ImageBrowserFragment() {
+
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
     }
     @Override
@@ -32,6 +38,21 @@ public class ImageBrowserFragment extends Fragment {
     }
     @Override
     public void onViewCreated(View view, Bundle savedInstances){
+        getFbAlbums();
+    }
 
+    private void getFbAlbums(){
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/albums",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        /* handle the result */
+                        Log.i(TAG, "Results: "+response.toString());
+                    }
+                }
+        ).executeAsync();
     }
 }
