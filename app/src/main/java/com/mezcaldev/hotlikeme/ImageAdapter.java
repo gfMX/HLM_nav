2f284ee1fcf2a22a9215db7767621afc26786fe5
gfMX <grafica.mx@gmail.com>
2016-06-27 12:41:09 -0500

    Gallery Working

---
+++ app/src/main/java/com/mezcaldev/hotlikeme/ImageAdapter.java
@@ -0,0 +1,60 @@
+package com.mezcaldev.hotlikeme;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by developer on 27/06/16.
+ */
+public class ImageAdapter extends ArrayAdapter {
+    private final String TAG_i = "Image Resource: ";
+
+    private List<String> imageUrls;
+
+    private Context mContext;
+    private LayoutInflater inflater;
+
+    // Constructor
+    public ImageAdapter(Context context, List<String> urls) {
+        super(context, R.layout.grid_item_layout, urls);
+
+        mContext = context;
+        imageUrls = urls;
+
+        Log.i(TAG_i,"URLS: " + imageUrls);
+
+        inflater = LayoutInflater.from(context);
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ImageView myImageView;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.grid_item_layout, parent, false);
+        } /*else {
+            myImageView = (ImageView) convertView;
+        }*/
+
+        Uri myUri = Uri.parse(imageUrls.get(position));
+
+        //Glide.load(myUri).into(myImageView).centerCrop().animate(R.anim.fade_in).begin();
+        Glide
+                .with(mContext)
+                .load(myUri)
+                .centerCrop()
+                .into((ImageView) convertView);
+
+        return convertView;
+    }
+
+}
--- app/src/main/java/com/mezcaldev/hotlikeme/ImageBrowserFragment.java
+++ app/src/main/java/com/mezcaldev/hotlikeme/ImageBrowserFragment.java
@@ -2,24 +2,24 @@ package com.mezcaldev.hotlikeme;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -29,6 +29,10 @@ public class ImageBrowserFragment extends Fragment {
     private static final String TAG = "FacebookLogin";
     private AccessToken accessToken;
     private String fieldsParams = "picture";
+
+    private GridView gridView;
+    static List<String> imUrls = new ArrayList<>();
+    static String[] imIds;
 
     public ImageBrowserFragment() {
 
@@ -40,7 +44,7 @@ public class ImageBrowserFragment extends Fragment {
         FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
 
         accessToken = AccessToken.getCurrentAccessToken();
-        Log.i(TAG, "Acces Token"+ accessToken.toString());
+        Log.i(TAG, "AccessToken"+ accessToken.toString());
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,7 +57,10 @@ public class ImageBrowserFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstances){
         getFbPhotos fbPhotos = new getFbPhotos();
         fbPhotos.execute();
+
+
     }
+
 
     private class getFbPhotos extends AsyncTask <Void, Void, Void>{
         @Override
@@ -99,8 +106,15 @@ public class ImageBrowserFragment extends Fragment {
                     JSONObject object1 = jsonArray1.getJSONObject(i);
                     String object2 = object1.get("picture").toString();
                     String object3 = object1.get("id").toString();
+                    imUrls.add(object2);
+                    //imIds[i] = object3;
+
                     //Log.i(TAG,"Object: " + object2 + " Id: " + object3);
+                    //Log.i(TAG, "New elements: " + imUrls.get(i));
                 }
+                //Log.i(TAG, "Elements: " + imUrls);
+                gridView = (GridView) getActivity().findViewById(R.id.gridView);
+                gridView.setAdapter(new ImageAdapter(getActivity(), imUrls));
             } else {
                 Log.i(TAG, "Nothing to do here!");
             }
