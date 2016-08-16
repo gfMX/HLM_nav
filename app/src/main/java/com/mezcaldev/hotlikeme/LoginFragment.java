a8b52bd4060cc2f743b1a457c2c8bfe1bb9bfb47
gfMX <grafica.mx@gmail.com>
2016-08-16 17:16:13 -0500

    Optimizing for credentials checks and redirecting User Activity

--- app/src/main/java/com/mezcaldev/hotlikeme/LoginFragment.java
+++ app/src/main/java/com/mezcaldev/hotlikeme/LoginFragment.java
@@ -166,9 +166,9 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //View view = inflater.inflate(R.layout.fragment_main, container, false);
+        //View view = inflater.inflate(R.layout.fragment_login, container, false);
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
     @Override
